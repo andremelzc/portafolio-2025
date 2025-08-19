c143f288a7fd5eb15013005e19a0235837aa7163
andremelzc <andre.melendezc@unmsm.edu.pe>
2025-08-19 00:53:07 -0500

    Add ContactSection and SkillsSection with buttons; create SkillCarousel component

--- app/components/sections/ContactSection.tsx
+++ app/components/sections/ContactSection.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import Button from "@/app/components/ui/Button";
 
 export default function ContactSection() {
-  return <section id="contacto">ContactSection</section>;
+  return (
+    <section id="contacto">
+      <div className="flex flex-col gap-16 text-center items-center">
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-2 ">
+            <h1 className="text-6xl">Contacto</h1>
+            <h2 className="text-xl">Escríbeme si quieres trabajar conmigo</h2>
+          </div>
+          <div className="flex flex-col gap-4 justify-center">
+            <Button variant="outline">Frontend</Button>
+            <Button variant="outline">Backend y DB</Button>
+            <Button variant="outline">Herramientas y Gestión</Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- app/components/sections/SkillsSection.tsx
+++ app/components/sections/SkillsSection.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import Button from "@/app/components/ui/Button";
 
 export default function SkillsSection() {
-  return <section id="habilidades">SkillsSection</section>;
+  return (
+    <section id="habilidades">
+      <div className="flex flex-col gap-16 text-center items-center">
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-2 ">
+            <h1 className="text-6xl">Habilidades</h1>
+            <h2 className="text-xl">
+              Explora mis habilidades en las siguientes áreas
+            </h2>
+          </div>
+          <div className="flex gap-4 justify-center">
+            <Button variant="outline">Frontend</Button>
+            <Button variant="outline">Backend y DB</Button>
+            <Button variant="outline">Herramientas y Gestión</Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
